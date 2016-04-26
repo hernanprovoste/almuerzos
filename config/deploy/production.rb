84ba83e59b5c1ec10d6b95823f795c1929c1fd1e
Hernan <hernanprovostev@outlook.com>
2016-04-26 17:49:46 -0300

    Modificación de capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '10.180.66.25', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
